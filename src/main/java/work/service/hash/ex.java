29e5dd459d58c7364573c32bd5a60f48154ae643
PHM-dung2 <pp4992@naver.com>
2025-03-10 03:14:04 +0900

    3/9 박희만 해쉬함수 구현중

---
+++ src/main/java/work/service/hash/ex.java
@@ -0,0 +1,20 @@
+package work.service.hash;
+
+import java.util.Scanner;
+
+public class ex {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str1 = scan.next();
+        String str2 = scan.next();
+
+        str1 = hash.hashStr(str1);
+
+        str2 = hash.hashStr(str2);
+
+        System.out.println(str1);
+        System.out.println(str2);
+        System.out.println(str1.equals(str2));
+    }
+
+}
