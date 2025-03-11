a331a1e617749dbf91d348a9625ed0cd6f64f63a
PHM-dung2 <pp4992@naver.com>
2025-03-11 18:01:17 +0900

    3/11 박희만 : Reprot CRUD 파일 생성[백엔드]

--- src/main/java/work/model/dto/MessageDto/MessageType.java
+++
@@ -1,9 +0,0 @@
-package work.model.dto.MessageDto;
-
-public enum MessageType {
-    OPEN,
-    SEND,
-    CLOSE,
-    // EXISTING은 출력 x
-    EXISTING
-}
---
+++ src/main/java/work/service/report/ReportService.java
@@ -0,0 +1,8 @@
+package work.service.report;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReportService {
+
+}
