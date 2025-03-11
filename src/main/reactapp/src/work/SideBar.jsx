3c08646b8c54e968614982e73e911629cdaf80dd
PHM-dung2 <pp4992@naver.com>
2025-03-12 08:26:15 +0900

    3/11 박희만 사이드바 레이아웃 구현중

--- src/main/reactapp/src/work/SideBar.jsx
+++ src/main/reactapp/src/work/SideBar.jsx
@@ -1,16 +1,27 @@
 import { Link } from "react-router-dom";
 
+/* mui */
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+
 export default function SideBar(){
     return(<>
-        <div>
-            <ul id="sideMenu" >
-                <li> <Link to="/ " > 홈 </Link> </li>
-                <li> <Link to="/chatting" > 메신저 </Link> </li>
-                <li> <Link to="/report_Write" > 보고서 현황 </Link> </li>
-                <li> <Link to="/report_View" > 보고서 작성 </Link> </li>
-                <li> <Link to="/board" > 사내 게시판 </Link> </li>
+        <div id="sideMenu">
+            <Container fixed maxWidth="xl" >
+                <Box sx={{ bgcolor: 'white', height: '100vh' }} >
+                    <Stack spacing={1} direction="column" sx={{ minWidth: 0}} >
+                        <Link to="/" className="logoLink" > <img src="logo.png" /> <span> 워크 플랫폼 </span> </Link>
+                        <Link to="/chatting"> <Button> 메신저 </Button> </Link>
+                        <Link to="/report_Write"> <Button> 보고서 작성 </Button> </Link>
+                        <Link to="/report_View"> <Button> 보고서 현황 </Button> </Link>
+                        <Link to="/board"> <Button> 사내 게시판 </Button> </Link>
+                        
+                    </Stack>
 
-            </ul>
+                </Box>
+            </Container>
         </div>
     </>)
 } // f end
