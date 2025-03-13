05ed4fd5aa05a5a175214fbc09c8279b9dc03607
kim ri won <lelabo7317@gmail.com>
2025-03-13 18:02:13 +0900

    3/13(김리원) : 최종 취합

--- src/main/java/work/controller/report/ApprovalController.java
+++ src/main/java/work/controller/report/ApprovalController.java
@@ -1,37 +1,37 @@
-package work.controller.report;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import work.model.dto.report.ApprovalDto;
-import work.service.report.ApprovalService;
-
-import java.util.List;
-
-@RestController
-@RequestMapping("/Approval")
-public class ApprovalController {
-
-    private final ApprovalService approvalService;
-
-    @Autowired
-    public ApprovalController(ApprovalService approvalService) {
-        this.approvalService = approvalService;
-    } // f end
-
-//    // 1. 승인 테이블 등록
-//    @PostMapping
-//    public boolean write(@RequestBody ApprovalDto approvalDto){
-//        System.out.println("ApprovalController.write");
-//        System.out.println("approvalDto = " + approvalDto);
-//        return approvalService.write(approvalDto);
+//package work.controller.report;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.*;
+//import work.model.dto.report.ApprovalDto;
+//import work.service.report.ApprovalService;
+//
+//import java.util.List;
+//
+//@RestController
+//@RequestMapping("/Approval")
+//public class ApprovalController {
+//
+//    private final ApprovalService approvalService;
+//
+//    @Autowired
+//    public ApprovalController(ApprovalService approvalService) {
+//        this.approvalService = approvalService;
 //    } // f end
 //
-//    // 2. 승인 전체 목록 조회
-//    @GetMapping
-//    public List<ApprovalDto> findAll(){
-//        System.out.println("ApprovalController.findAll");
-//        return approvalService.findAll();
-//    } // f end
-
-
-}
+////    // 1. 승인 테이블 등록
+////    @PostMapping
+////    public boolean write(@RequestBody ApprovalDto approvalDto){
+////        System.out.println("ApprovalController.write");
+////        System.out.println("approvalDto = " + approvalDto);
+////        return approvalService.write(approvalDto);
+////    } // f end
+////
+////    // 2. 승인 전체 목록 조회
+////    @GetMapping
+////    public List<ApprovalDto> findAll(){
+////        System.out.println("ApprovalController.findAll");
+////        return approvalService.findAll();
+////    } // f end
+//
+//
+//}
--- src/main/java/work/model/mapper/report/ApprovalMapper.java
+++ src/main/java/work/model/mapper/report/ApprovalMapper.java
@@ -1,7 +1,7 @@
-package work.model.mapper.report;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public class ApprovalMapper {
-}
+//package work.model.mapper.report;
+//
+//import org.apache.ibatis.annotations.Mapper;
+//
+//@Mapper
+//public class ApprovalMapper {
+//}
