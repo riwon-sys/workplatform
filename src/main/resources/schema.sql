b796fb2155038d9a4cbec06570fe82a2fb10f818
kimdohaaa <jjeenee0408@gmail.com>
2025-03-13 15:12:38 +0900

    (3/13) 도하 : 브라우저 접속 소켓 구현중

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,24 @@
-DROP TABLE IF EXISTS member;
+-- 결재 테이블 삭제 (approval 테이블은 fileshare를 참조하므로, 먼저 삭제)
 DROP TABLE IF EXISTS approval;
+
+-- 파일 공유 테이블 삭제 (fileshare 테이블은 paritcipant를 참조하므로, fileshare 테이블을 먼저 삭제)
+DROP TABLE IF EXISTS fileshare;
+
+-- 메시지 테이블 삭제 (message 테이블은 paritcipant를 참조하므로, message 테이블을 먼저 삭제)
+DROP TABLE IF EXISTS message;
+
+-- 참여자 테이블 삭제 (paritcipant 테이블은 room과 member를 참조하므로, paritcipant 테이블을 삭제)
+DROP TABLE IF EXISTS paritcipant;
+
+-- 보고서 테이블 삭제 (report 테이블은 approval을 참조하므로, 먼저 삭제)
 DROP TABLE IF EXISTS report;
+
+-- 채팅방 테이블 삭제 (room 테이블은 member를 참조하므로, room 테이블을 마지막에 삭제)
+DROP TABLE IF EXISTS room;
+
+-- 이제 'member' 테이블 삭제
+DROP TABLE IF EXISTS member;
+
 
 -- 직원 테이블 생성
 CREATE TABLE member (
@@ -15,6 +33,50 @@ CREATE TABLE member (
     constraint PRIMARY KEY (mno)
 );
 
+-- 채팅방 테이블
+create table room (
+        rno int unsigned auto_increment,
+    rname varchar(50) not null,
+    rtype varchar(30) not null,
+    rdate datetime default now(),
+    rlastdate datetime,
+    rstate boolean default true,
+    mno int unsigned,
+    constraint primary key (rno),
+    constraint foreign key (mno) references member (mno) on update cascade on delete cascade
+);
+
+-- 참여자 테이블
+create table paritcipant(
+        pno int unsigned auto_increment,
+    pdate datetime default now(),
+    mno int unsigned,
+    rno int unsigned,
+    constraint primary key (pno),
+    constraint foreign key (mno) references member (mno) on update cascade on delete cascade,
+    constraint foreign key (rno) references room (rno) on update cascade on delete cascade
+);
+-- 메세지 테이블
+create table message(
+        msno int unsigned auto_increment,
+    msg text ,
+    msdate datetime default now(),
+    msstate int default 0,
+    pno int unsigned,
+    constraint primary key (msno),
+    constraint foreign key (pno) references paritcipant (pno) on update cascade on delete cascade
+);
+
+--파일 테이블
+create table fileshare(
+        fno int unsigned auto_increment,
+    fname varchar(30),
+    flocation varchar(255),
+    fdate datetime default now(),
+    pno int unsigned,
+    constraint primary key (fno),
+    constraint foreign key (pno) references paritcipant (pno) on update cascade on delete cascade
+);
 -- 보고서 테이블
 create table report(
 	rpno int unsigned auto_increment,
@@ -29,6 +91,7 @@ create table report(
     constraint primary key( rpno ),
     constraint foreign key( mno ) references member ( mno ) on update cascade on delete cascade
 );
+
 
 -- 결재 테이블
 create table approval(
