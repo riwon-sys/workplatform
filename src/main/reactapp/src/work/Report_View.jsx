d63f2e4da6fb911f7ddea5d176581c6604cbd8af
PHM-dung2 <pp4992@naver.com>
2025-03-14 18:23:30 +0900

    3/14 박희만 : report 목록 조회 기능 구현중 [ 백, 프론트 ]

--- src/main/reactapp/src/work/Report_View.jsx
+++ src/main/reactapp/src/work/Report_View.jsx
@@ -2,13 +2,14 @@ import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid2';
+import Report_List from './Report_List';
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: '#fff',
     ...theme.typography.body2,
     padding: theme.spacing(1),
     textAlign: 'center',
-    color: theme.palette.text.secondary,
+    color: theme.palette.text.primary,
     height: '100%', // 높이 설정 추가
 }));
 
@@ -17,14 +18,17 @@ export default function Report_View() {
         <Box sx={{ flexGrow: 1, height: '100vh', display: 'flex', flexDirection: 'column' }}>
           <Grid container spacing={0} sx={{ height: '100%' }}> 
             {/* size: 너비 조정 */}
-            <Grid size={3.5} sx={{ height: '100%' }}> 
-              <Item>size=3.5</Item>
+            <Grid size={5} sx={{ height: '100%' }}> 
+              <Item>
+                <h1> 보고서 목록 </h1>
+                <br/>
+                <Report_List />
+              </Item>
             </Grid>
-            <Grid size={5} sx={{ height: '100%' }}>
-              <Item>size=5</Item>
-            </Grid>
-            <Grid size={3.5} sx={{ height: '100%' }}>
-              <Item>size=3.5</Item>
+            <Grid size={7} sx={{ height: '100%' }}>
+              <Item>
+                
+              </Item>
             </Grid>
           </Grid>
         </Box>
