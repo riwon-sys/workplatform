cf0005e6e3a5ba750369f0f0b12ff51269c83293
PHM-dung2 <pp4992@naver.com>
2025-03-17 15:15:01 +0900

    3/17 박희만 : 보고서 상세목록 리액트 구현중

--- src/main/reactapp/src/work/Report_View.jsx
+++ src/main/reactapp/src/work/Report_View.jsx
@@ -3,12 +3,13 @@ import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid2';
 import Report_List from './Report_List';
+import Button from '@mui/material/Button';
 
 import Report_Form from './Report_Form';
 import * as React from 'react';
 import { StyledEngineProvider, CssVarsProvider } from '@mui/joy/styles';
 import { useParams } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: '#fff',
@@ -34,8 +35,13 @@ export default function Report_View() {
       rpexpected: '' 
     });
   
-  const formDataChange = (e) => {
-    setFormData( { ...formData, [ e.target.name ] : e.target.value } )
+  useEffect( () => { onFindByRpno(); }, [] )
+
+  const onFindByRpno = async ( props ) => {
+    try{
+      const response = await axios.get( `http://localhost:8080/report/view?rpno=${rpno}` );
+      setFormData( response.data );
+    }catch( e ){ console.log( e ) }
   } // f end
 
   return (
@@ -63,10 +69,10 @@ export default function Report_View() {
                 <Report_Form formData={ formData } isReadOnly={ false } rpno={ rpno } />
  
                 <div style={{ display: 'flex', justifyContent: 'flex-end' }} >
-                  <Button variant="contained" color="info" sx={{ mt: 3 }} >
+                  <Button variant="contained" color="info" sx={{ mt: 3, ml: 3 }} >
                       수정
                   </Button>
-                  <Button variant="contained" color="info" sx={{ mt: 3 }} >
+                  <Button variant="contained" color="info" sx={{ mt: 3, ml: 3 }} >
                       삭제
                   </Button>
                 </div>
