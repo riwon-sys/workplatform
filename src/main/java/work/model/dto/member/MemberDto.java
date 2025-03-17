ea81f9e508a81c7879d51b32043bbd6f8941380c
kim ri won <lelabo7317@gmail.com>
2025-03-17 17:57:21 +0900

    3/17(김리원) : 생성자 수정 , data.sql(수정) ,로그인 세션처리 기능 추가 (컨트롤러,서비스,매퍼 수정) , Talend API TEST 결과 이상 없음

--- src/main/java/work/model/dto/member/MemberDto.java
+++ src/main/java/work/model/dto/member/MemberDto.java
@@ -2,19 +2,19 @@ package work.model.dto.member;
 
 import lombok.*;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
-@ToString
-@Builder
+@NoArgsConstructor     // 빈 생성자
+@AllArgsConstructor  // 전체 생성자
+@Builder            // 빌더패턴 지원
+
+@Getter @Setter @ToString
+
 public class MemberDto {
-    private int mno; // unsigned : 직별부여 및 사번부여( 1: 인사팀 2: 마케팅팀 3: 영업팀 4: 운영팀 5:기술팀 6:디자인팀 7:재무팀) (맨 앞자리 부서/ 자바에서부여)
-    private  String mpwd; // default '1234' :  비밀번호 기본값 설정
-    private  String mname; // not null : 사원 이름
-    private  String mphone; // not null unique : 사원 전화번호, 고유값 설정
-    private  String memail; // not null : 사원 사내 이메일
-    private  int mtype; // default 0 : 사원 현재 상태 (0: 활동, 1: 부재, 2: 외부업무, 3: 퇴사)
-    private  String mrank; // not null 사원 직급 : 사원 - 대리 - 팀장 - 부서장
-    private  String mprofile; // default 'default.jpg' : 프로필 사진 기본값 설정
+    private int mno;                // unsigned : 직별부여 및 사번부여( 1: 인사팀 2: 마케팅팀 3: 영업팀 4: 운영팀 5:기술팀 6:디자인팀 7:재무팀) (맨 앞자리 부서/ 자바에서부여)
+    private  String mpwd;           // default '1234' :  비밀번호 기본값 설정
+    private  String mname;          // not null : 사원 이름
+    private  String mphone;         // not null unique : 사원 전화번호, 고유값 설정
+    private  String memail;         // not null : 사원 사내 이메일
+    private  int mtype;             // default 0 : 사원 현재 상태 (0: 활동, 1: 부재, 2: 외부업무, 3: 퇴사)
+    private  String mrank;          // not null 사원 직급 : 사원 - 대리 - 팀장 - 부서장
+    private  String mprofile;       // default 'default.jpg' : 프로필 사진 기본값 설정
 }
