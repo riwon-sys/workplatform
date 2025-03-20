96ae387b1ecdb61546c895fa87d5fae76ce9f284
kim ri won <lelabo7317@gmail.com>
2025-03-20 15:16:18 +0900

    3/19(김리원) : 1. 설치 npm install redux-persist(리덕스 퍼시스턴스 설정) 2. 설치 implementation 'org.springframework.security:spring-security-crypto:6.4.3'(비크립트 설정)

--- src/main/reactapp/src/work/App.jsx
+++ src/main/reactapp/src/work/App.jsx
@@ -11,11 +11,12 @@ import Report_Write from './Report_Write.jsx';
 import Report_View from './Report_View.jsx';
 import Board from './Board.jsx';
 import theme from './theme';
+import ChatTeset from "../chatsocket/chat/ChattingTest.jsx";
+
 import Member_Post from './member/Member_Post.jsx'; // rw
 /* css */
 import './App.css';
-
-import ChatTeset from "../chatsocket/chat/ChattingTest.jsx";
+import Report_Update from "./Report_Update.jsx";
 
 export default function Test( props ){
     return(<>
@@ -30,7 +31,11 @@ export default function Test( props ){
                     <Route path="/chatting" element={ <ChatTeset /> }></Route>
                     <Route path="/report/write" element={ <Report_Write /> }></Route>
                     <Route path="/report/view" element={ <Report_View /> }></Route>
+                    <Route path="/report/view/:rpno" element={ <Report_View /> }></Route>
                     <Route path="/board" element={ <Board /> }></Route>
+                    <Route path="/report/list" element={ <Report_List /> } ></Route>
+                    <Route path="/report/Form" element={ <Report_Form /> } ></Route>
+                    <Route path="/report/update/:rpno" element={ <Report_Update /> } ></Route>
                     <Route path="/member/post" element={<Member_Post/> }></Route>
                 </Routes>
             </Box>
