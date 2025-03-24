dc152c21e9022cfeeb1a6c2c88a6ad3f0a15d105
jeenimari <gha0189@gmail.com>
2025-03-24 18:17:42 +0900

    2025/03/24

---
+++ src/main/reactapp/src/work/component/report/PostModal.jsx
@@ -0,0 +1,107 @@
+import * as React from 'react';
+
+/* mui import */
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import SignatureCanvas from 'react-signature-canvas'
+
+/* mui icon */
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 450,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function PostModal( { signCanvas, onPost, onApproval, btnName } ) {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  // 서명 전체 지우기
+  const onClear = () => {
+    if( !confirm('서명을 지우시겠습니까?') ){ return; }
+    signCanvas.current.clear();
+  } // f end
+
+  return (
+    <div>
+      <Button 
+        variant="contained" 
+        color="info" sx={{ mt: 3 }} 
+        onClick={handleOpen}>
+            { btnName }
+      </Button>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={open}
+        closeAfterTransition
+        slots={{ backdrop: Backdrop }}
+        slotProps={{
+          backdrop: {
+            timeout: 400,
+          },
+        }}
+      >
+        <Fade in={open}>
+          <Box sx={style}>
+            <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+              <Typography id="transition-modal-title" variant="h6" component="h2">
+                보고서 서명해주세요.
+              </Typography>
+              <IconButton color="primary" aria-label="add to shopping cart">
+                <CloseIcon onClick={ handleClose } />
+              </IconButton>
+            </Box>
+
+            <Box 
+              sx={{ 
+                width: '100%', 
+                border: '1px solid #e0e0e0',
+                margin: '0 auto',
+                width: 210, 
+                padding: 1,
+              }}>
+              <SignatureCanvas
+                ref={ signCanvas }
+                canvasProps={{ 
+                  className: 'signCanvas',
+                  width: 190,
+                }}
+              />
+            </Box>
+
+            <div style={{ display: 'flex', justifyContent: 'flex-end' }} >
+              {
+                btnName == "작성" ?
+                  <Button variant="contained" color="info" sx={{ mt: 2 }} onClick={ onPost } >
+                    등록
+                  </Button> :
+                  <Button variant="contained" color="info" sx={{ mt: 2 }} onClick={ onApproval } >
+                    승인
+                  </Button>
+              }
+              <Button variant="contained" color="info" sx={{ mt: 2, ml: 3 }} onClick={ onClear } >
+                전체 지우기
+              </Button>
+            </div>
+            
+          </Box>
+        </Fade>
+      </Modal>
+    </div>
+  );
+}
---
+++ src/main/reactapp/src/work/component/report/Report_List.jsx
@@ -0,0 +1,66 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+/* mui import */
+import Table from '@mui/joy/Table';
+
+export default function Report_List( { rpno, reports, page, setReports, setPage, setTotalPages } ) {
+  
+  if( rpno == null ){ rpno = '' };
+
+  useEffect( () => { onFindByMno( page ); }, [ page ] );
+
+  const onFindByMno = async ( page ) => {
+    try{
+      const response = await axios.get( `http://localhost:8080/api/report?page=${page}&pageSize=10`, { withCredentials : true } )
+      setReports( response.data.list );
+      setPage( response.data.pageNum );
+      setTotalPages( response.data.pages );
+    }catch( e ){ console.log(e); }
+  } // f end
+
+  const navigate = useNavigate();
+  const onView = async ( rpno ) => { await navigate( `/report/view/${rpno}` ); }
+
+  return (<>
+    <div style={{ height: '640px' }} >
+      <Table 
+        hoverRow 
+        // variant='outlined'
+        sx={{ 
+          '& th' : { textAlign: 'center', backgroundColor: '#e5edf7' }, 
+          '& td' : { height: '60px' }
+        }} 
+      >
+        <thead>
+          <tr>
+            <th style={{ width: '10%' }}> 번호 </th>
+            <th style={{ width: '40%' }}> 보고서명 </th>
+            <th style={{ width: '25%' }}> 보고자명 </th>
+            <th style={{ width: '25%' }}> 날짜 </th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+          reports.length > 0 ?
+            reports.map( ( row )  => (
+              <tr key = { row.rpno } onClick={ () => onView( row.rpno ) } 
+                style={{ backgroundColor: rpno == row.rpno ? '#eeeeee'  : '' }}>
+                <td> { row.rpno } </td>
+                <td style={{ textAlign: 'left' }} > { row.rpname } </td>
+                <td style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexWrap: 'wrap' }} > 
+                  { row.mname } <div> ( { row.mdepartment } ) </div>  
+                </td>
+                <td> { row.rpdate } </td>
+              </tr>
+            )) :
+              <tr>
+                <td colSpan={4} > 작성한 보고서가 없습니다. </td>
+              </tr>
+          }
+        </tbody>
+      </Table>
+    </div>
+  </>);
+}
