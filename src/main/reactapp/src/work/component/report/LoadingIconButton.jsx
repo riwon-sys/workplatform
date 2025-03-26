a6f7dcc041196115f3776e1f2a79433e174d7666
PHM-dung2 <pp4992@naver.com>
2025-03-26 20:30:29 +0900

    3/25 박희만 : pdf 버튼 프론트 수정

--- src/main/reactapp/src/work/component/report/LoadingIconButton.jsx
+++ src/main/reactapp/src/work/component/report/LoadingIconButton.jsx
@@ -23,12 +23,13 @@ export default function LoadingIconButton( { convertToPdf } ) {
         variant="contained"
         color="info"
         sx={{
-          width: "45px", // 가로 너비 설정
+          width: "70px", // 가로 너비 설정
           minWidth: "unset", // 기본 min-width 제거
           padding: "8px", // 패딩 적용
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
+          mb: 2
         }}
         disabled={loading} // 로딩 중이면 버튼 비활성화
       >
